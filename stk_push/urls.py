97536ac80461aba9d7baef870eac965a7d186b7e
Ezra <holla@apensoftwares.co.ke>
2024-12-03 17:19:42 +0300

    updated

--- stk_push/urls.py
+++ stk_push/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Form page
+
     path('stk_push/', views.stk_push, name='stk_push'),
+    
     path('callback', views.callback, name='callback'),
      path('waiting/<int:transaction_id>/', views.waiting_page, name='waiting_page'),
     path('check-status/<int:transaction_id>/', views.check_status, name='check_status'),
@@ -11,6 +13,5 @@ urlpatterns = [
     path('payment-failed/', views.payment_failed, name='payment_failed'),
     path('payment-cancelled/', views.payment_failed, name='payment_cancelled'),
     path('view_payments/', views.view_payments, name='view_payments'),
-
   
 ]
--- stk_push/views.py
+++ stk_push/views.py
@@ -15,16 +15,14 @@ class MpesaPassword:
     def generate_security_credential():
         timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
         
-        # Safaricom credentials
-        business_short_code = '174379'  # Your shortcode here
+       
+        business_short_code = '174379'  
         passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'  
         data_to_encode = business_short_code + passkey + timestamp
         online_password = base64.b64encode(data_to_encode.encode()).decode('utf-8')
 
         return online_password
 
-
-# Safaricom daraja credentials
 CONSUMER_KEY = 'jkXBgjW27IKhblgEVsR5SOfTh6Am0MZ3IWs1xTPIyKgLA70x'  
 CONSUMER_SECRET = 'z6WM04Fv88SyG8QeSgtva1GXdNyWPpbOi6vTNuEbuA6aL64l6sqAzxqpePAEXYvG'  
 SHORTCODE = '174379'  
@@ -35,41 +33,6 @@ def generate_access_token():
     auth_url = f'{BASE_URL}/oauth/v1/generate?grant_type=client_credentials'
     response = requests.get(auth_url, auth=(CONSUMER_KEY, CONSUMER_SECRET))
     return response.json().get('access_token')
-
-
-def query_transaction(transaction_id):
-    access_token = generate_access_token()
-    status_url = f'{BASE_URL}/mpesa/transactionstatus/v1/query'
-
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-        'Content-Type': 'application/json',
-    }
-
-    security_credential = MpesaPassword.generate_security_credential()
-
-    payload = {
-        "Initiator": 'testapi',  
-        "SecurityCredential": security_credential,  
-        "CommandID": "TransactionStatusQuery",
-        "TransactionID": transaction_id,
-        "PartyA": SHORTCODE,
-        "IdentifierType": "1", 
-        "ResultURL": "https://b6ec-217-199-146-223.ngrok-free.app/callback/transaction_status/",
-        "QueueTimeOutURL": "https://b6ec-217-199-146-223.ngrok-free.app/callback/transaction_timeout/",
-        "Remarks": "Checking transaction status",
-        "Occasion": "STK Push"
-    }
-
-    response = requests.post(status_url, json=payload, headers=headers)
-    return response.json()
-
-
-def index(request):
-    return render(request, 'index.html')
-
-
-
 
 @csrf_exempt
 def stk_push(request):
@@ -217,12 +180,9 @@ def callback(request):
 
     return JsonResponse({"error": "Invalid request method"}, status=400)
 
-
-
 def waiting_page(request, transaction_id):
     transaction = Transaction.objects.get(id=transaction_id)
     return render(request, 'waiting.html', {'transaction_id': transaction_id})
-
 
 def check_status(request, transaction_id):
     transaction = Transaction.objects.filter(id=transaction_id).first()
@@ -239,25 +199,20 @@ def check_status(request, transaction_id):
     else:
         return JsonResponse({"status": "Unknown", "message": "Transaction is still being processed or status is unknown"})
 
-
 def payment_success(request):
     return render(request, 'payment_success.html')
-
-
 
 def payment_failed(request):
     return render(request, 'payment_failed.html')
 
-
 def payment_cancelled(request):
     return render(request, 'payment_cancelled.html')
-
 
 def view_payments(request):
     search_query = request.GET.get('q', '')
     page_number = request.GET.get('page', 1)
 
-    # Filter transactions based on search query
+   
     payments = Transaction.objects.filter(
         phone_number__icontains=search_query
     ) | Transaction.objects.filter(
@@ -268,10 +223,10 @@ def view_payments(request):
         mpesa_receipt_number__icontains=search_query
     )
 
-    paginator = Paginator(payments, 10)  # 10 transactions per page
+    paginator = Paginator(payments, 10)  
     page_obj = paginator.get_page(page_number)
 
-    # Check if the request expects a JSON response
+   
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':  
         payments_list = [
             {
